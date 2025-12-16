3465172e671ac36000d915257aa762b9fe74fb22
Okhotsk-hr <hrkiha183.service@gmail.com>
2025-12-16 15:32:05 +0900

    リモコン/数字キーは判別

--- data/RC/RC.ino
+++ data/RC/RC.ino
@@ -1,8 +1,8 @@
 #include <IRremote.h>
 
-#define IR_RECEIVE_PIN 11   // IR受信機のOUTピン
+#define IR_RECEIVE_PIN 11  // IR受信機のOUTピン
 
-uint32_t lastCode = 0;      // 前回のコード
+uint32_t lastCode = 0;  // 前回のコード
 
 void setup() {
   Serial.begin(9600);
@@ -18,12 +18,34 @@ void loop() {
     uint32_t code = IrReceiver.decodedIRData.decodedRawData;
 
     if (code != 0 && code != lastCode) {
-      Serial.print("Received: 0x");
-      Serial.println(code, HEX);
+      switch (code) {
+        case 0xFFA25D: Serial.println("POWER"); break;
+        case 0xFFE21D: Serial.println("FUNC / STOP"); break;
+        case 0xFF629D: Serial.println("VOL +"); break;
+        case 0xFFA857: Serial.println("VOL -"); break;
+        case 0xFF906F: Serial.println("UP"); break;
+        case 0xFFE01F: Serial.println("DOWN"); break;
+        case 0xE916FF00: Serial.println("0"); break;
+        case 0xF30CFF00: Serial.println("1"); break;
+        case 0xE718FF00: Serial.println("2"); break;
+        case 0xA15EFF00: Serial.println("3"); break;
+        case 0xF708FF00: Serial.println("4"); break;
+        case 0xE31CFF00: Serial.println("5"); break;
+        case 0xA55AFF00: Serial.println("6"); break;
+        case 0xBD42FF00: Serial.println("7"); break;
+        case 0xAD52FF00: Serial.println("8"); break;
+        case 0xB54AFF00: Serial.println("9"); break;
+        default:
+          Serial.print("UNKNOWN: 0x");
+          Serial.println(code, HEX);
+          break;
+      }
+      // Serial.print("Received: 0x");
+      // Serial.println(code, HEX);
 
-      lastCode = code;   // 今回のコードを保存
+      lastCode = code;  // 今回のコードを保存
     }
 
-    IrReceiver.resume(); // 次の受信へ
+    IrReceiver.resume();  // 次の受信へ
   }
 }
